f289f01ef0453c423e464d3fc3f54e5fce0272dd
prashanth-kadari <prashanth.kadari@allianz.de>
2021-03-11 15:24:48 +0530

    AVC Test

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace gitdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fetch test");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("merge test 2");
+            Console.WriteLine("Git Demo for branch");
+            Console.WriteLine("Git Demo for branch2");
+        }
+    }
+}
---
+++ branch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitdemo
+{
+    class branch
+    {
+        public void pull()
+        {
+            Console.WriteLine("change from demo branch");
+            Console.WriteLine("created a new branch named master");
+            Console.WriteLine("change 6");
+            Console.WriteLine("Issue demo");
+        }
+    }
+}
